0d28655f79c5516e8fcfca72d1cd87d7e4b737ca
Đặng Văn Đức <ducdangvan@vccorp.vn>
2019-01-07 11:40:30 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 01/07/2019
+ * Time: 11:38 AM
+ */
+?>
+Use of Indexes to Satisfy ORDER BY
+ORDER BY column nào thì nên index column đấy
